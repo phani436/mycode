ec30ef08f7c22c5ad8b74fb2bc41887bbd7608b1
Ck <chaithanyakodurii@gmail.com>
2018-12-10 18:48:07 -0500

    moving and renaming

---
+++ lab_input/input_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#user_input = input("Please enter your full name: ")
+#print("your full name is:" + user_input)
+
+#!/usr/bin/env python3
+# Written By: Alice Anderson
+# Collect IP address information from a user, and display it back to them.
+
+## Collect input from the user
+user_input = input('Please enter an IPv4 IP address:') # user_input stores the IPv4 ip address
+
+## Print out the input collected from the user
+print("You told me the IPv4 address is:" + user_input)
