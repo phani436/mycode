1e8cfed028bdf1080b70b13a09e6b2e027453869
Ck <chaithanyakodurii@gmail.com>
2018-12-11 02:11:21 -0500

    if-test2

---
+++ if-test/if-hostname.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+hostname = input('please enter the hostname: ')
+if hostname == 'MTG':
+  print('The hostname was found to be mtg')
---
+++ if-test2/if-test2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+ipchk = input('Apply an IP address: ')
+
+if ipchk:
+   print('Looks like the IP address was set: ' + ipchk)
